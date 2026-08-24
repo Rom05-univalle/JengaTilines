1cb424e354b4601b971a73bea9e64efef488e3a1
Mauricio <lcm0035247@gmail.com>
2026-08-24 13:23:53 -0400

    prueba

--- JengaN/Assets/Scripts/JengaUIManager.cs
+++ JengaN/Assets/Scripts/JengaUIManager.cs
@@ -6,6 +6,7 @@ public class JengaUIManager : MonoBehaviour
     private GameObject uiCanvas;
     
     // UI Elements References
+    // nuevo comentario
     private Text turnText;
     private Image turnBackground;
     private GameObject trackingWarningPanel;
